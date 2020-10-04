22987d337a5d16baf34d61c9f67c671f7bc32c72
RicardoM99160 <inuriqui_cool2@outlook.com>
2020-10-04 13:29:38 -0600

    modal funcional 90%

--- src/app/modal-mascota/modal-mascota.component.ts
+++ src/app/modal-mascota/modal-mascota.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { Cliente } from '../models/cliente';
+import { Mascota } from '../models/mascota';
+import { DatosService } from '../services/datos.service';
 
 @Component({
   selector: 'app-modal-mascota',
@@ -7,9 +10,15 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./modal-mascota.component.css']
 })
 export class ModalMascotaComponent {
+
+  cliente:Cliente;
+  nombreMascota:string = "";
   closeResult = '';
 
-  constructor(private modalService: NgbModal) {}
+  constructor(
+    private modalService: NgbModal,
+    private servicioDatos:DatosService
+    ) {}
 
   open(content) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
@@ -17,7 +26,25 @@ export class ModalMascotaComponent {
     }, (reason) => {
       this.closeResult = ``;
     });
+    this.cliente = JSON.parse(localStorage.getItem('cliente')) as Cliente;
+    this.nombreMascota = "";
   }
+
+  agregarMascota(){
+    if(this.cliente.mascotas[0].nombre = "vacio"){
+      this.cliente.mascotas.pop();
+    }
+    let m = new Mascota;
+    m.nombre = this.nombreMascota;
+    m.consultas = 0;
+    console.log(m);
+    this.cliente.mascotas.push(m);
+    console.log(this.cliente);
+    this.servicioDatos.agregarCliente(this.cliente.DUI, this.cliente);
+    this.nombreMascota = "";
+  }
+
+
 
   private getDismissReason(reason: any): string {
     if (reason === ModalDismissReasons.ESC) {
@@ -28,4 +55,6 @@ export class ModalMascotaComponent {
       return `with: ${reason}`;
     }
   }
+
+
 }
